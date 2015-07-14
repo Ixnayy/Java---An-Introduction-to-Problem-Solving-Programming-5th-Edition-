b8a365ddf20551eb3c90c8e58c575c529649b9ec
Ixnayy <samedward1723@yahoo.com>
2015-07-13 21:22:16 -0400

    Chapter 3, Programming Project 3
    Write a program that reads three strings from the keyboard. Although the
    strings are in no particular order, display the string that would be
    second if they were arranged lexicographically.

---
+++ CH3. PP. 3/CH3. PP. 3.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a program that reads three strings from the keyboard. Although the strings are in no particular order, display the string 
+ * that would be second if they were arranged lexicographically.
+ * 
+ * Name: Sam Fragapane III
+ * Version: 1.0.0
+ */
+import java.util.Scanner;
+public class Exercise3
+{
+   public static void main(String[] args)
+   {
+       System.out.println("Please enter three strings, one per each line");
+       Scanner keyboard = new Scanner(System.in);
+       String x = keyboard.nextLine();
+       String y = keyboard.nextLine();
+       String z = keyboard.nextLine();
+       String min = x;
+       String max = x;
+       String middle = "";
+       
+      if (min.compareTo(y) > 0)
+       min = y;
+      if (min.compareTo(z) > 0)
+       min = z;
+      if (max.compareTo(y) < 0)
+       max = y;
+      if (max.compareTo(z) < 0)
+       max = z;
+      if (!x.equals(min) && !x.equals(max))
+       middle = x;
+      if (!y.equals(min) && !y.equals(max))
+       middle = y;
+      if (!z.equals(min) && !z.equals(max))
+       middle = z;
+      
+      System.out.println(middle);
+    }
+}
